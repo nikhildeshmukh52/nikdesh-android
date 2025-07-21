e16bdd0957c3498832c6e3fc084b3cdb1d5bcc26
Nikhil.Deshmukh <Nikhil.Deshmukh@partner.bmw.de>
2025-07-21 13:17:41 +0200

    Broadcast Sender

--- app/src/main/java/com/android/androidinternals/MainActivity.kt
+++ app/src/main/java/com/android/androidinternals/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.android.androidinternals
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +23,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Button(
+                        onClick = {
+                            val intent = Intent("com.android.androidinternals.ACTION_GREETING").apply {
+                                `package` = "com.android.androidinternals"
+                            }
+                            sendBroadcast(intent)
+                        },
+                        modifier = Modifier.padding(innerPadding).fillMaxSize().wrapContentSize()
+                    ) {
+                        Text(text = "Send Greeting Broadcast")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAppTheme {
-        Greeting("Android")
     }
 }
