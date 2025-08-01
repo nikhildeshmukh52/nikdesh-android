61cb815c9a9dcefd2cf84e9cffc8c3d023f2d5e5
Nikhil.Deshmukh <Nikhil.Deshmukh@partner.bmw.de>
2025-08-01 12:34:12 +0200

    app specific files in external storage

---
+++ app/src/main/java/com/android/androidinternals/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.android.androidinternals
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.lifecycle.ViewModel
+import java.io.File
+import java.util.UUID
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.withContext
+
+class MainViewModel: ViewModel() {
+
+    private val _bitmap = MutableStateFlow<Bitmap?>(null)
+    val bitmap = _bitmap.asStateFlow()
+
+    private var file: File? = null
+
+    fun saveToExternalStorage(context: Context, bitmap: Bitmap): Boolean {
+        val externalDirectory = context.getExternalFilesDir(null)
+        val directory = File(externalDirectory, "AndroidExternalStorage")
+        if(!directory.exists()) {
+            directory.mkdir()
+        }
+
+        file = File(directory, UUID.randomUUID().toString() + ".jpg")
+        return file!!.outputStream().use { outputStream->
+            val compressed = bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            compressed
+        }
+    }
+
+    suspend fun readImageFromExternalStorage(): Boolean {
+        return withContext(Dispatchers.IO) {
+            file?.let {
+                file!!.inputStream().use { inputStream ->
+                    val bytes = inputStream.readBytes()
+                    _bitmap.value = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                }
+                true
+            } ?: false
+        }
+    }
+}
